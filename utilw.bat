584514d9af940371382156255fc8fbe4c25555cf
Yong <work.qu@outlook.com>
2019-08-16 17:13:37 +0800

    keep shell alive when return error code

--- utilw.bat
+++ utilw.bat
@@ -1,5 +1,2 @@
 @echo off
 @python %~dp0\buildutil.py %*
-set BUILD_EXIT_CODE=%errorlevel%
-echo exitcode = %BUILD_EXIT_CODE%
-exit %BUILD_EXIT_CODE%
--- utilw.sh
+++ utilw.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 cur_dir=`dirname $0`
 python ${cur_dir}/buildutil.py $*
-BUILD_EXIT_CODE=$?
-echo exitcode = $BUILD_EXIT_CODE
-exit $BUILD_EXIT_CODE
